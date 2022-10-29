1bd2a4ba0d79a234bfd752a0303eaff8fa5bfeaf
LAPTOP-VE24BJF4\guill <m.eugenia.damy@gmail.com>
2022-10-29 12:53:20 -0300

    to be continued

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
-// import "./styles.css";
+import "./index.css";
 
 type Product = {
   id: number;
   name: string;
-  brand: string;
+  marca: string;
   price: number;
 };
 
 function App() {
   const [products, setProducts] = useState<Product[]>();
-  const [inputs, setInputs] = useState({ name: "", brand: "", price: 0 });
+  const [inputs, setInputs] = useState({ name: "", marca: "", price: 0 });
   useEffect(() => {
-    fetch("https://expresspostman-production.up.railway.app/product").then(
+    fetch("https://back-production-04ac.up.railway.app/product").then(
       (result) => {
         result.json().then((data: Product[]) => {
           setProducts(data);
@@ -23,7 +23,7 @@ function App() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch("https://expresspostman-production.up.railway.app/product", {
+    fetch("https://back-production-04ac.up.railway.app/product", {
       method: "POST",
       body: JSON.stringify({ ...inputs, price: Number(inputs.price) }),
       headers: {
@@ -53,7 +53,7 @@ function App() {
               <div key={product.id} className="m-1">
                 <div className="bg-primary p-1">
                   <p>{product.name}</p>
-                  <p>{product.brand}</p>
+                  <p>{product.marca}</p>
                   <p>{product.price}</p>
                 </div>
               </div>
@@ -63,7 +63,7 @@ function App() {
       </div>
       <form onSubmit={handleSubmit}>
         <input placeholder={"name"} name={"name"} onChange={handleChange} />
-        <input placeholder={"brand"} name={"brand"} onChange={handleChange} />
+        <input placeholder={"marca"} name={"marca"} onChange={handleChange} />
         <input
           placeholder={"price"}
           type={"number"}
